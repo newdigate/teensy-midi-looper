9caf4e49c9265de5179911b8fac9459480e118fe
Nic Newdigate <nicnewdigate@gmail.com>
2018-01-13 16:53:24 +0000

    Update MidiWriter.cpp

--- MidiWriter.cpp
+++ MidiWriter.cpp
@@ -9,35 +9,85 @@ const byte header[] = {
      0x4d, 0x54, 0x72, 0x6B
 };
 
-
 MidiWriter::MidiWriter(){
 }
 
+
+void MidiWriter::write_buf_int(unsigned int data) {
+  //Serial.printf("%x\n", data);
+  byte b = data >> 24;
+  //Serial.printf("%x.", b);
+  write_buf_byte( b );
+  
+  b = data >> 16;
+  //Serial.printf("%x.", b);
+  write_buf_byte( b );
+   
+  b = data >> 8;
+  //Serial.printf("%x.", b);
+  write_buf_byte( b );
+
+  b = data;
+  //Serial.printf("%x\n", b);
+  write_buf_byte( b );
+}
+
+void MidiWriter::write_buf_byte(byte b) {
+  if (_bufferPos > 40) {
+    flush();
+  }
+  _buffer[_bufferPos++] = b;
+}
+    
 void MidiWriter::setFilename(const char* filename) {
   flush();
-  dataFile = SD.open(filename, FILE_WRITE);
+  
+  sprintf(_filename, "%s.mid", filename);
+  
+  int count = 1;
+  while (SD.exists(_filename)) {
+    //Serial.printf("'%s' already exists...\n", _filename);   
+    sprintf(_filename, "%s%i.mid", filename, count);
+    count++;
+  }
+  //Serial.printf("using filename '%s'\n", _filename);
 }
 
 void MidiWriter::writeHeader() {
   for (unsigned int i=0; i<sizeof(header); i++)
-    dataFile.write(header[i]);
+    write_buf_byte(header[i]);
+    flush();
 }
 
 void MidiWriter::addEvent(int ticks, byte a, byte b, byte c, byte d) {
-  dataFile.write(ticks);
-  dataFile.write(a);
-  dataFile.write(b);
-  dataFile.write(c);
-  dataFile.write(d);
-  trackSize += 4;
+  write_buf_int(ticks);
+  write_buf_byte(a);
+  write_buf_byte(b);
+  write_buf_byte(c);
+  write_buf_byte(d);
+  trackSize += 8;
 }
 
 void MidiWriter::flush() {
-  if (dataFile != NULL) {
-    dataFile.flush();
-    unsigned long position = dataFile.position();
-    dataFile.seek(18);
-    dataFile.write(trackSize);
-    dataFile.seek(position);
+  if (_bufferPos == 0) return;
+  
+  File data = SD.open(_filename, FILE_WRITE);
+  if (!data) {
+    //Serial.printf("Not able to open %s\n", _filename);
+    return;
   }
+  
+  for (byte b = 0; b < _bufferPos; b++) {
+    data.write(_buffer[b]);
+  } 
+  _bufferPos = 0;
+  
+  data.seek(18);
+  write_buf_int(trackSize);
+  for (byte b = 0; b < _bufferPos; b++) {
+    data.write(_buffer[b]);
+  } 
+  _bufferPos = 0;
+  
+  data.close();
 }
