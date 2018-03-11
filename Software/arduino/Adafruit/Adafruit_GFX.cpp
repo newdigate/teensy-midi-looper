a4e639f3d44f295cb63f86e33322c425c3ef5e5b
Nicholas Newdigate <nicnewdigate@Nicholass-MBP.home>
2018-03-11 13:10:33 +0000

    refacted midi into arduino abstractions

--- Software/arduino/Adafruit/Adafruit_GFX.cpp
+++ Software/arduino/Adafruit/Adafruit_GFX.cpp
@@ -852,9 +852,9 @@ size_t Adafruit_GFX::write(uint8_t c) {
         }
 
     }
-#if ARDUINO >= 100
+//#if ARDUINO >= 100
     return 1;
-#endif
+//#endif
 }
 
 void Adafruit_GFX::setCursor(int16_t x, int16_t y) {
