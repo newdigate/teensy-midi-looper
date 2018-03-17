660e8b37cc364c57d00c9b142f77cc70c85c0d62
Nicholas Newdigate <nicnewdigate@Nicholass-MBP.home>
2018-03-17 01:38:47 +0000

    progress 1

---
+++ Software/xcode/TFTDisplayEmulator/Source/Adafruit_GFX_juce.hpp
@@ -0,0 +1,70 @@
+//
+//  Adafruit_GFX_juce.hpp
+//  TFTDisplayEmulator - App
+//
+//  Created by Nicholas Newdigate on 11/03/2018.
+//  Copyright © 2018 ROLI Ltd. All rights reserved.
+//
+
+#ifndef Adafruit_GFX_juce_hpp
+#define Adafruit_GFX_juce_hpp
+
+#include <stdio.h>
+#include <Adafruit/Adafruit_GFX.h>
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class Adafruit_GFX_juce : public Adafruit_GFX {
+public:
+    inline Adafruit_GFX_juce(int16_t w, int16_t h, juce::Image &image) : Adafruit_GFX(w, h) {
+        _image = &image;
+    }
+    
+    inline int write(uint8_t u) {
+        return 0;
+    }
+    
+    inline int write(unsigned char const*, unsigned long) {
+        return 0;
+    }
+    
+    inline void drawPixel(int16_t x, int16_t y, uint16_t color) {
+        unsigned r = (color & 0xF800) >> 11;
+        unsigned g = (color & 0x07E0) >> 5;
+        unsigned b = color & 0x001F;
+        
+        r = (r * 255) / 31;
+        g = (g * 255) / 63;
+        b = (b * 255) / 31;
+        _image->setPixelAt(x, y, juce::Colour(r, g, b));
+    }
+    
+    //inline void startWrite(void) {}
+    //inline void writePixel(int16_t x, int16_t y, uint16_t color) {}
+    //inline void writeFillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color) {}
+    //inline void writeFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color) {}
+    //inline void writeFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color) {}
+    //inline void writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color) {}
+    //inline void endWrite(void) {}
+    
+    // CONTROL API
+    // These MAY be overridden by the subclass to provide device-specific
+    // optimized code.  Otherwise 'generic' versions are used.
+    //inline void setRotation(uint8_t r) {}
+    //inline void invertDisplay(bool i) {}
+    
+    // BASIC DRAW API
+    // These MAY be overridden by the subclass to provide device-specific
+    // optimized code.  Otherwise 'generic' versions are used.
+    //inline void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color) {}
+    //inline void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color) {}
+    //inline void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color) {}
+
+    //inline void fillScreen(uint16_t color) {}
+    // Optional and probably not necessary to change
+    //inline void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color) {}
+    //inline void drawRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color) {}
+private:
+    juce::Image *_image;
+};
+
+#endif /* Adafruit_GFX_juce_hpp */
---
+++ Software/xcode/TFTDisplayEmulator/Source/MainComponent.cpp
@@ -0,0 +1,86 @@
+/*
+  ==============================================================================
+
+   This file is part of the JUCE library.
+   Copyright (c) 2017 - ROLI Ltd.
+
+   JUCE is an open source library subject to commercial or open-source
+   licensing.
+
+   By using JUCE, you agree to the terms of both the JUCE 5 End-User License
+   Agreement and JUCE 5 Privacy Policy (both updated and effective as of the
+   27th April 2017).
+
+   End User License Agreement: www.juce.com/juce-5-licence
+   Privacy Policy: www.juce.com/juce-5-privacy-policy
+
+   Or: You may also use this code under the terms of the GPL v3 (see
+   www.gnu.org/licenses).
+
+   JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include <TFTPianoDisplay.h>
+#include <Adafruit/Adafruit_GFX.h>
+#include "Adafruit_GFX_juce.hpp"
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainContentComponent   : public AnimatedAppComponent
+{
+public:
+    //==============================================================================
+    MainContentComponent()
+    {
+        setSize (_width, _height);
+        setFramesPerSecond (1);
+        //image.setPixelAt(0, 0, juce::Colour(100,100,250));
+    }
+
+    void update() override
+    {
+        ff.drawPiano();
+        // This function is called at the frequency specified by the setFramesPerSecond() call
+        // in the constructor. You can use it to update counters, animate values, etc.
+    }
+
+    void paint (Graphics& g) override
+    {
+        g.fillRect(0, 0, 160, 160);
+        juce::AffineTransform scale = juce::AffineTransform(AffineTransform::scale(10.0, 10.0));
+        g.drawImage ( image,
+                    0, 0, getWidth(), getHeight(),
+                      0, 0, 160, 160, false);
+    }
+
+    void resized() override
+    {
+        // This is called when the MainContentComponent is resized.
+        // If you add any child components, this is where you should
+        // update their positions.
+    }
+
+
+private:
+    //==============================================================================
+    const int _width = 160;
+    const int _height = 160;
+    // Your private member variables go here...
+    juce::Image image = Image(juce::Image::PixelFormat::RGB, _width, _height, true);
+    Byte octaves = 2, startOctave = 2, x = 10, y = 10;
+    Adafruit_GFX_juce juceTFT = Adafruit_GFX_juce(160, 160, image);
+    TFTPianoDisplay ff = TFTPianoDisplay(juceTFT, octaves, startOctave, x, y);
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
+
+
+// (This function is called by the app startup code to create our main component)
+Component* createMainContentComponent()    { return new MainContentComponent(); }
