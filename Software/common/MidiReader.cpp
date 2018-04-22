59f674dd353b92d899468216aaec72bce5e1744a
Nic Newdigate <nicnewdigate@gmail.com>
2018-04-22 13:55:30 +0100

    midi reading...

--- Software/common/MidiReader.cpp
+++ Software/common/MidiReader.cpp
@@ -7,9 +7,11 @@
 #if ARDUINO >= 100
 #include "Arduino.h"
 #include <SD.h>
+#include <midi_Message.h>
 #else
 #include "../arduino/mock_arduino.h"
 #include "SD/SD.h"
+#include "midi/midi_Message.h"
 #endif
 
 #define buffer_size 50
@@ -23,6 +25,7 @@ struct smf_header {
     uint16_t division;
 };
 //#pragma pack(pop, r1)
+uint32_t varfieldGet(File &file, uint16_t &progress);
 
 bool MidiReader::open(const char *filename) {
     if (!SD.exists(filename))
@@ -33,7 +36,7 @@ bool MidiReader::open(const char *filename) {
     if (!_midifile)
         return false;
 
-    char buffer[6];
+    unsigned char buffer[6];
     if (_midifile.read(buffer, 4) != 4) {
         return false;
     }
@@ -77,7 +80,80 @@ bool MidiReader::open(const char *filename) {
 
         _track_offset[i] = _midifile.position();
         _track_size[i] = track_length;
+        _midifile.seek(track_length);
+
+        _track_buffer[i] = new Queue(sizeof(MidiMessage), TRACK_EVENT_BUFFER_SIZE, FIFO);
+        _track_position[i] = 0;
+
+        while (_track_position[i] < track_length && !_track_buffer[i]->isFull()) {
+
+            uint16_t progress = 0;
+            uint32_t delta_ticks = varfieldGet(_midifile, progress);
+            _track_position[i] += progress;
+
+            _midifile.read(buffer, 1);
+            _track_position[i]++;
+            unsigned char status_byte = buffer[0];
+
+            switch (status_byte & 0xF0) {
+                case 0x80:
+                case 0x90:
+                    //bool noteOn = (status_byte & 0xF0 == 0x80);
+                    //unsigned char channel = status_byte & 0x0F;
+
+                    _midifile.read(buffer, 1);
+                    _track_position[i]++;
+                    unsigned char key = buffer[0];
+
+                    _midifile.read(buffer, 1);
+                    _track_position[i]++;
+                    unsigned char velocity = buffer[0];
+
+                    MidiMessage midiMessage = MidiMessage();
+                    midiMessage.status = status_byte;
+                    midiMessage.delta_ticks = delta_ticks;
+                    midiMessage.key = key;
+                    midiMessage.velocity = velocity;
+                    _track_buffer[i]->push(&midiMessage);
+
+                    break;
+
+            }
+        }
+
     }
+    _pulses_per_quater_note = header.division;
+
 
     return true;
+}
+
+bool MidiReader::read(unsigned char trackNumber) {
+    return false;
+}
+
+uint32_t varfieldGet(File &file, uint16_t &progress)
+{
+    uint32_t ret = 0;
+    uint8_t byte_in;
+
+    for (;;)
+    {
+        if (file.available())
+            // return error
+            byte_in = file.read();
+        ret = (ret << 7) | (byte_in & 0x7f);
+        if (!(byte_in & 0x80))
+            return ret;
+    }
+}
+
+
+bool MidiReader::seek(unsigned char trackNumber, unsigned long milliseconds) {
+
+    float quarter_note = _current_beats_per_minute * milliseconds / (1000 * 60);
+    unsigned long ticks = quarter_note * _pulses_per_quater_note;
+    Serial.print(quarter_note);
+
+    return false;
 }
--- Software/common/MidiReader.h
+++ Software/common/MidiReader.h
@@ -16,6 +16,15 @@
 #include <string>
 
 #define MAX_MIDI_TRACKS 16
+#define TRACK_EVENT_BUFFER_SIZE 50
+#define TRACK_EVENT_BUFFER_MIN_THRESHOLD 20
+
+struct MidiMessage {
+    uint32_t delta_ticks;
+    unsigned char status;
+    unsigned char key;
+    unsigned char velocity;
+};
 
 class MidiReader {
 public:
@@ -24,19 +33,24 @@ public:
             _midifile(),
             _initialized(false),
             _track_offset(),
-            _track_size()
-    {
-    }
+            _track_size(),
+            _track_buffer() {
+    };
 
     bool open(const char* filename);
-
+    bool seek(unsigned char trackNumber, unsigned long milliseconds);
+    bool read(unsigned char trackNumber);
 
 private:
     bool _initialized;
     File _midifile;
     char* _filename;
+    uint16_t _pulses_per_quater_note;
+    float _current_beats_per_minute = 120.0;
     unsigned long _track_size[MAX_MIDI_TRACKS];
     unsigned long _track_offset[MAX_MIDI_TRACKS];
+    unsigned long _track_position[MAX_MIDI_TRACKS];
+    Queue *_track_buffer[MAX_MIDI_TRACKS];
 };
 
 
