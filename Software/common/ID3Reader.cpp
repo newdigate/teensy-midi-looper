28729b778b7a6a1d16129b2173d8ef4ccc86144d
Nic Newdigate <nicnewdigate@gmail.com>
2018-05-07 23:59:01 +0100

    mp3 id3 tag reading

--- Software/common/ID3Reader.cpp
+++ Software/common/ID3Reader.cpp
@@ -1,33 +1,92 @@
-//
-// Created by Nicholas Newdigate on 10/04/2018.
-//
+#include "ID3Reader.h"
 
-#ifndef TEENSY_MIDI_COMMON_ID3READER_H
-#define TEENSY_MIDI_COMMON_ID3READER_H
+bool ID3Reader::open(const char *filename) {
 
-#if ARDUINO >= 100
-#include "Arduino.h"
-#include <SD.h>
-#else
-#include "mock_arduino.h"
-#include "SD/SD.h"
-#endif
+    if (!SD.exists(filename))
+        return false;
 
-#include <string>
+    _file = SD.open(filename);
 
-class Id3Reader {
-public:
-    Id3Reader() :
-            _filename(),
-            _file() {
-    };
+    if (!_file)
+        return false;
 
-    bool open(const char* filename);
+    unsigned char buffer[10];
+    if (_file.read(buffer, 3) != 3) {
+        return false;
+    }
 
-private:
-    File _file;
-    char* _filename;
-};
+    if (buffer[0] != 'I' || buffer[1] != 'D' || buffer[2] != '3')
+        return false;
 
+    if (_file.read(buffer, 2) != 2) {
+        return false;
+    }
 
-#endif //TEENSY_MIDI_COMMON_ID3READER_H
+    unsigned char versionMajor = buffer[0];
+    unsigned char versionMinor = buffer[1];
+
+    if (_file.read(buffer, 1) != 1) {
+        return false;
+    }
+    unsigned char flags = buffer[1];
+
+    if (_file.read(buffer, 4) != 4) {
+        return false;
+    }
+
+    std::size_t s1;
+    std::size_t s2;
+    std::size_t s3;
+    std::size_t s4;
+
+    s1   = static_cast< std::size_t >( buffer[ 0 ] );
+    s2   = static_cast< std::size_t >( buffer[ 1 ] );
+    s3   = static_cast< std::size_t >( buffer[ 2 ] );
+    s4   = static_cast< std::size_t >( buffer[ 3 ] );
+    s1  &= 0x7F;
+    s2  &= 0x7F;
+    s3  &= 0x7F;
+    s4  &= 0x7F;
+
+    unsigned long header_length = (s1 << 21) | (s2 << 14) | (s3 << 7) | s4;
+
+    while( _file.position() < header_length + 10) {
+
+        char frameId[4];
+        if (_file.read(frameId, 4) != 4) {
+            return false;
+        }
+
+        if (_file.read(buffer, 4) != 4) {
+            return false;
+        }
+
+        s1   = static_cast< std::size_t >( buffer[ 0 ] );
+        s2   = static_cast< std::size_t >( buffer[ 1 ] );
+        s3   = static_cast< std::size_t >( buffer[ 2 ] );
+        s4   = static_cast< std::size_t >( buffer[ 3 ] );
+        s1  &= 0x7F;
+        s2  &= 0x7F;
+        s3  &= 0x7F;
+        s4  &= 0x7F;
+
+        unsigned long frame_length = (s1 << 21) | (s2 << 14) | (s3 << 7) | s4;
+
+        char flags[2];
+
+        if (_file.read(flags, 2) != 2) {
+            return false;
+        }
+
+        if (memcmp(frameId, "TIT2", 4) == 0) {
+            std::cout << "title:  ";
+            // title
+            char *text = new char[frame_length];
+            _file.read(text, frame_length);
+            std::cout << text << std::endl;
+            delete [] text;
+        }
+    }
+
+    return true;
+}
--- Software/common/ID3Reader.h
+++ Software/common/ID3Reader.h
@@ -2,8 +2,8 @@
 // Created by Nicholas Newdigate on 10/04/2018.
 //
 
-#ifndef TEENSY_MIDI_COMMON_MIDIREADER_H
-#define TEENSY_MIDI_COMMON_MIDIREADER_H
+#ifndef TEENSY_MIDI_COMMON_ID3READER_H
+#define TEENSY_MIDI_COMMON_ID3READER_H
 
 #if ARDUINO >= 100
 #include "Arduino.h"
@@ -15,48 +15,19 @@
 
 #include <string>
 
-#define MAX_MIDI_TRACKS 16
-#define TRACK_EVENT_BUFFER_SIZE 50
-#define TRACK_EVENT_BUFFER_MIN_THRESHOLD 20
-
-struct MidiMessage {
-    uint32_t delta_ticks;
-    unsigned char status;
-    unsigned char key;
-    unsigned char velocity;
-    unsigned char channel;
-};
-
-class MidiReader {
+class ID3Reader {
 public:
-    MidiReader() :
+    ID3Reader() :
             _filename(),
-            _midifile(),
-            _initialized(false),
-            _track_offset(),
-            _track_size(),
-            _track_buffer() {
+            _file() {
     };
 
     bool open(const char* filename);
-    bool seek(unsigned char trackNumber, unsigned long milliseconds);
-    bool read(unsigned char trackNumber);
 
 private:
-    bool _initialized;
-    File _midifile;
+    File _file;
     char* _filename;
-    uint16_t _pulses_per_quater_note;
-    float _current_beats_per_minute = 120.0;
-    unsigned long _track_size[MAX_MIDI_TRACKS];
-    unsigned long _track_offset[MAX_MIDI_TRACKS];
-    unsigned long _track_position[MAX_MIDI_TRACKS];
-    Queue *_track_buffer[MAX_MIDI_TRACKS];
-
-    void readMetaText(uint16_t track_index);
-
-    const char * voice_message_status_name(unsigned char status);
 };
 
 
-#endif //TEENSY_MIDI_COMMON_MIDIREADER_H
+#endif //TEENSY_MIDI_COMMON_ID3READER_H
